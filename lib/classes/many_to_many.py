b9c59b18ec66c4a4d3cbcb8007b3c90fb07047b0
wamwea <patokinya12@gmail.com>
2024-06-01 23:00:36 +0300

    complete lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,129 +1,58 @@
 class Article:
-    all = []
-
+    _all_articles = []
     def __init__(self, author, magazine, title):
-
-        self._author = author
-        self._magazine = magazine
-        self._title = title
-        Article.all.append(self)
-        author._articles.append(self)
-        magazine._articles.append(self)
-
-    @property
-    def author(self):
-        return self._author
-
-    @author.setter
-    def author(self, author):
-        if not isinstance(author, Author):
-            raise ValueError("Author must be of type Author")
-        self._author = author
-
-    @property
-    def magazine(self):
-        return self._magazine
-
-    @magazine.setter
-    def magazine(self, magazine):
-        if not isinstance(magazine, Magazine):
-            raise ValueError("Magazine must be of type Magazine")
-        self._magazine = magazine
-
-    @property
-    def title(self):
-        return self._title
-
-    def set_title(self, title):
-        raise AttributeError("Cannot change the title after the article is instantiated.")
-
-    title = property(fget=lambda self: self._title, fset=set_title)
-
+        self.author = author
+        self.magazine = magazine
+        self.title = title
+        Article._all_articles.append(self)
 class Author:
-    def __init__(self, name='Unknown'):
-        if not isinstance(name, str) or len(name) == 0:
-            raise ValueError("Name must be a non-empty string.")
-        
-        self._name = name
-        self._articles = []
-
-    @property
-    def name(self):
-        return self._name
-
-    def set_name(self, name):
-        raise AttributeError("Cannot change the name after the author is instantiated.")
+    def __init__(self, name):
+        self.name = name
 
     def articles(self):
-        return self._articles
+        return[article for article in Article._all_articles if article.author == self]
 
     def magazines(self):
-        return list(set(article.magazine for article in self._articles))
+        return list(set(article.magazine for article in self.articles()))
 
     def add_article(self, magazine, title):
-        return Article(self, magazine, title)
+        return Article(self,magazine,title)
 
     def topic_areas(self):
-        return list(set(article.magazine.category for article in self._articles))
+        return list(set(magazine.category for magazine in self.magazines()))
 
 class Magazine:
-    all_magazines = []
-
+    _all_magazines = []
     def __init__(self, name, category):
-        if not isinstance(name, str) or not (2 <= len(name) <= 16):
-            raise ValueError("Name must be a string between 2 and 16 characters, inclusive")
-        if not isinstance(category, str) or len(category) == 0:
-            raise ValueError("Category must be a non-empty string")
-
-        self._name = name
-        self._category = category
-        self._articles = []
-        Magazine.all_magazines.append(self)
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        if not isinstance(name, str) or not (2 <= len(name) <= 16):
-            raise ValueError("Name must be a string between 2 and 16 characters, inclusive")
-        self._name = name
-
-    @property
-    def category(self):
-        return self._category
-
-    @category.setter
-    def category(self, category):
-        if not isinstance(category, str) or len(category) == 0:
-            raise ValueError("Category must be a non-empty string")
-        self._category = category
+        self.name = name
+        self.category = category
+        Magazine._all_magazines.append(self)
 
     def articles(self):
-        return self._articles
+        return [article for article in Article._all_articles if article.magazine == self]
 
     def contributors(self):
-        return list(set(article.author for article in self._articles))
-
-    def article_titles(self):
-        return [article.title for article in self._articles]
+        return list(set(article.author for article in self.articles()))
 
     def contributing_authors(self):
+        # Return a list of authors who have written more than 2 articles for this magazine
         author_counts = {}
-        for article in self._articles:
+        for article in self.articles():
             author = article.author
             if author in author_counts:
                 author_counts[author] += 1
             else:
                 author_counts[author] = 1
-
         return [author for author, count in author_counts.items() if count > 2]
 
     @classmethod
     def top_publisher(cls):
-        if not cls.all_magazines:
-            return None
-        return max(cls.all_magazines, key=lambda magazine: len(magazine.articles()))
-
-
+        # Return the Magazine instance with the most articles
+        max_articles = 0
+        top_magazine = None
+        for magazine in cls._all_magazines:
+            num_articles = len(magazine.articles())
+            if num_articles > max_articles:
+                max_articles = num_articles
+                top_magazine = magazine
+        return top_magazine
